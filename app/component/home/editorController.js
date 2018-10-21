e2951cac9e160bb2a00bf1f453716fecbc5f5146
HuynhVuong87 <hv198197@gmail.com>
2018-10-21 18:37:40 +0700

    update

--- app/component/home/editorController.js
+++ app/component/home/editorController.js
@@ -1,28 +1,46 @@
-app.controller("editorController", function ($scope) {  
-    $scope.ctrl2 = "huynhnhon"
-    var canvas = document.getElementById("canvas");
-    var ctx = canvas.getContext("2d");
+app.controller("editorController", function($scope) {
+  $scope.ctrl2 = "huynhnhon"
+  var canvas = document.getElementById("canvas");
+  var ctx = canvas.getContext("2d");
 
-    var angleInDegrees = 0;
-    var image = document.getElementById("imgPre")
+  var angleInDegrees = 0;
+  var image = document.getElementById("imgPre")
 
-    function drawRotated(degrees) {
+  function drawRotated(degrees) {
 
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        ctx.save();
-        ctx.translate(canvas.width / 2, canvas.height / 2);
-        ctx.rotate(degrees * Math.PI / 180);
-        ctx.drawImage(image, -image.width / 2, -image.height / 2);
-        ctx.restore();
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.save();
+    ctx.translate(canvas.width / 2, canvas.height / 2);
+    ctx.rotate(degrees * Math.PI / 180);
+    ctx.drawImage(image, -image.width / 2, -image.height / 2);
+    ctx.restore();
 
+  }
+
+  $scope.resize = function() {
+    $scope.width = canvas.width
+    $scope.height = canvas.height
+    console.log($scope.width)
+    $('div#resize').modal()
+    $scope.saveResize = function() {
+      setSizeCanvas($scope.width, $scope.height)
+      ctx.drawImage(image, 0, 0, $scope.width, $scope.height)
     }
-    $scope.rotate = function () {
+  }
 
-        var newW = $('canvas').attr("width")
-        var newH = $('canvas').attr("height")
-        $('canvas').attr("width", newH)
-        $('canvas').attr("height", newW)
-        angleInDegrees-=90
-        drawRotated(angleInDegrees);
-    }
+  $scope.rotate = function() {
+    var newW = $('canvas').attr("width")
+    var newH = $('canvas').attr("height")
+    setSizeCanvas(newH, newW)
+    angleInDegrees -= 90
+    drawRotated(angleInDegrees);
+  }
+
+  function setSizeCanvas(w, h) {
+    $('canvas').attr("width", w)
+    $('canvas').attr("height", h)
+    $('div.main').css({
+      "width": Number(w) + 20 + "px"
+    })
+  }
 })
--- app/component/home/homeController.js
+++ app/component/home/homeController.js
@@ -1,138 +1,157 @@
-app.controller("homeController", function ($scope) {
+app.controller("homeController", function($scope) {
 
-    var dropZone = document.getElementById('drop-zone');
+  var dropZone = document.getElementById('drop-zone');
 
-    var n = {
-        layout: 'topRight',
-        progressBar: true,
-        theme: 'relax',
-        type: 'success',
-        closeWith: ['button']
-    }
+  var n = {
+    layout: 'topRight',
+    progressBar: true,
+    theme: 'relax',
+    type: 'success',
+    closeWith: ['button']
+  }
 
-    var noty
+  var noty
 
-    $scope.uploaded = false
+  $scope.uploaded = false
 
-    var canvas = document.getElementById("canvas");
-    var ctx = canvas.getContext("2d");
+  var canvas = document.getElementById("canvas");
+  var ctx = canvas.getContext("2d");
+  var image = document.getElementById("imgPre")
 
-    var image = document.getElementById("imgPre")
-
-    function loadImg() {
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        image.onload = function () {
-
-            $('canvas').attr("width", this.width)
-            $('canvas').attr("height", this.height)
-            ctx.drawImage(image, 0, 0)
-
-            // ctx.drawImage(image, 0, 0, image.width, image.height, // source rectangle
-            //     0, 0, cw, ch);
-        }
-        // image.src = url;
-    }
-    document.addEventListener('paste', function (e) {
-        if ($("input#pasteUrl").is(":focus")) {
-
-
-            noty ? noty.close() : ""
-            setTimeout(() => {
-                var pastedText = $("input#pasteUrl").val();
-                console.log(pastedText);
-                loadImg();
-                $scope.uploaded = true;
-                $scope.linkImage = pastedText;
-                $scope.$apply();
-                n.text = pastedText
-                noty = new Noty(n).show();
-                $("input#pasteUrl").val("");
-            }, 500)
-
-                
-
-        }
+  $scope.close = function() {
+    $scope.uploaded = false;
+    $scope.linkImage = ''
+    $('div.main').css({
+      "width": "unset"
     })
-    var startUpload = function (file) {
+    $('#imgPre').css("width", "unset")
+  }
 
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
+  function loadImg() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    image.onload = function() {
 
-        noty ? noty.close() :""            
-        console.log(file);
+      if (this.width > 1200) {
+        alert("kích thước ảnh quá lớn(>1200px)")
+        $('canvas').css("display", "none")
+        $('#imgPre').css({
+          "display": "block"
+        })
+        $('#imgPre').css("width", "100%")
+      } else {
+        $('canvas').attr("width", this.width)
+        $('canvas').attr("height", this.height)
+        $('div.main').css({
+          "width": this.width + 20 + "px"
+        })
+        ctx.drawImage(image, 0, 0)
+      }
 
-        var pro = 0
-        var timer = setInterval(function () {
-            pro++
-            $('div.progress-bar').attr('aria-valuenow', pro).css('width', pro + "%");
 
-            if (pro == 10) {}
-            if (pro == 60) {
-                clearInterval(timer);
-            }
+      // ctx.drawImage(image, 0, 0, image.width, image.height, // source rectangle
+      //     0, 0, cw, ch);
+    }
+    // image.src = url;
+  }
+  document.addEventListener('paste', function(e) {
+    if ($("input#pasteUrl").is(":focus")) {
+      noty ? noty.close() : ""
+      setTimeout(() => {
+        var pastedText = $("input#pasteUrl").val();
+        console.log(pastedText);
+        loadImg();
+        $scope.uploaded = true;
+        $scope.linkImage = pastedText;
+        $scope.$apply();
+        n.text = pastedText
+        noty = new Noty(n).show();
+        $("input#pasteUrl").val("");
+      }, 500)
 
-        }, 60)
 
-        var settings = {
-            // async: false,
-            crossDomain: true,
-            processData: false,
-            contentType: false,
-            type: 'POST',
-            url: 'https://api.imgur.com/3/image',
-            headers: {
-                Authorization: 'Client-ID 03338436af088fa',
-                Accept: 'application/json'
-            },
-            mimeType: 'multipart/form-data'
-        };
-
-        var formData = new FormData();
-        formData.append("image", file);
-        settings.data = formData;
-
-        $.ajax(settings).done(function (response) {
-            clearInterval(timer)
-            var obj = JSON.parse(response)
-            console.log(obj.data);
-            pro = 100
-            $('div.progress-bar').attr('aria-valuenow', 100).css('width', "100%");
-            
-            var promise = new Promise(function (resolve, reject) {
-                loadImg()
-                resolve()
-            })
-            promise.then(() => {
-                $scope.linkImage = obj.data.link
-                $scope.uploaded = true
-                $scope.$apply()
-                n.text =obj.data.link
-                noty = new Noty(n).show()
-            })
-
-        });
 
     }
+  })
+  var startUpload = function(file) {
 
-    dropZone.ondrop = function (e) {
-        e.preventDefault();
-        this.className = 'upload-drop-zone';
-        startUpload(e.dataTransfer.files[0])
-    }
-    $("#js-upload-files").on('change', function () {
-        startUpload($(this).get(0).files[0])
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    noty ? noty.close() : ""
+    console.log(file);
+
+    var pro = 0
+    var timer = setInterval(function() {
+      pro++
+      $('div.progress-bar').attr('aria-valuenow', pro).css('width', pro + "%");
+
+      if (pro == 10) {}
+      if (pro == 60) {
+        clearInterval(timer);
+      }
+
+    }, 60)
+
+    var settings = {
+      // async: false,
+      crossDomain: true,
+      processData: false,
+      contentType: false,
+      type: 'POST',
+      url: 'https://api.imgur.com/3/image',
+      headers: {
+        Authorization: 'Client-ID 03338436af088fa',
+        Accept: 'application/json'
+      },
+      mimeType: 'multipart/form-data'
+    };
+
+    var formData = new FormData();
+    formData.append("image", file);
+    settings.data = formData;
+
+    $.ajax(settings).done(function(response) {
+      clearInterval(timer)
+      var obj = JSON.parse(response)
+      console.log(obj.data);
+      pro = 100
+      $('div.progress-bar').attr('aria-valuenow', 100).css('width', "100%");
+
+      var promise = new Promise(function(resolve, reject) {
+        loadImg()
+        resolve()
+      })
+      promise.then(() => {
+        $scope.linkImage = obj.data.link
+        $scope.uploaded = true
+        $scope.$apply()
+        n.text = obj.data.link
+        noty = new Noty(n).show()
+      })
+
     });
 
-    $scope.uploadHandle = function () {
-        $("#js-upload-files").trigger('click');
-    }
+  }
 
-    dropZone.ondragover = function () {
-        this.className = 'upload-drop-zone drop';
-        return false;
-    }
+  dropZone.ondrop = function(e) {
+    e.preventDefault();
+    this.className = 'upload-drop-zone';
+    startUpload(e.dataTransfer.files[0])
+  }
+  $("#js-upload-files").on('change', function() {
+    startUpload($(this).get(0).files[0])
+  });
 
-    dropZone.ondragleave = function () {
-        this.className = 'upload-drop-zone';
-        return false;
-    }
+  $scope.uploadHandle = function() {
+    $("#js-upload-files").trigger('click');
+  }
+
+  dropZone.ondragover = function() {
+    this.className = 'upload-drop-zone drop';
+    return false;
+  }
+
+  dropZone.ondragleave = function() {
+    this.className = 'upload-drop-zone';
+    return false;
+  }
 })
